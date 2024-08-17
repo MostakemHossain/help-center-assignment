866b403264ab1bee3e99b5dc0b4127a16a9b3516
MostakemHossain <mostakememon123@gmail.com>
2024-08-17 18:15:34 +0600

    navbar added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,11 @@
+import Navbar from "@/components/Navbar";
+
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="">
+      <Navbar />
+    </div>
+  );
 };
 
 export default page;
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { BsArrowUpRightCircle } from "react-icons/bs";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-black rounded-tr-[10px] rounded-tl-[10px] text-white p-4 flex justify-center md:gap-[650px] items-center">
+      <div className="flex items-center space-x-4">
+        <div className="">
+          <BsArrowUpRightCircle className="h-6 w-6 " />
+        </div>
+        <Link href="/" className="text-lg font-semibold">
+          Abstract
+        </Link>
+        <span className="text-gray-500">|</span>
+        <a href="#" className="text-lg">
+          Help Center
+        </a>
+      </div>
+      <div>
+        <button className="bg-gray-800 hover:bg-gray-700 text-white font-semibold py-2 px-4 rounded-md">
+          Submit a request
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
