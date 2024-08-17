a66d67869cb9900b997e4e1f01bdd4dde0cd5e86
MostakemHossain <mostakememon123@gmail.com>
2024-08-17 20:14:55 +0600

    cards show

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,13 @@
+import Cards from "@/components/Cards/Cards";
 import Navbar from "@/components/Navbar";
+import SearchHero from "@/components/SearchHero";
 
 const page = () => {
   return (
     <div className="">
       <Navbar />
+      <SearchHero />
+      <Cards />
     </div>
   );
 };
