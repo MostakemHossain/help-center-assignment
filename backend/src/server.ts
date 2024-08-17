e29b9e9c9413ffbf7ad4246a3b4a2c3fe2942dcb
MostakemHossain <mostakememon123@gmail.com>
2024-08-17 13:49:33 +0600

    some modification

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,36 +1,11 @@
-import { Server } from "http";
-import mongoose from "mongoose";
 import app from "./app";
 import config from "./app/config";
-
-let server: Server;
-const port = config.port;
+const port = 8500;
 
 async function main() {
-  try {
-    await mongoose.connect(config.database_url as string);
-
-    server = app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
-    });
-  } catch (err) {
-    console.log(err);
-  }
+  app.listen(config.port, () => {
+    console.log(`Server running on port ${config.port}`);
+  });
 }
 
 main();
-
-process.on("unhandledRejection", () => {
-  console.log(`😈🙉 unhandledRejection is detected. Shutting down...`);
-  if (server) {
-    server.close(() => {
-      process.exit(1);
-    });
-  }
-  process.exit(1);
-});
-
-process.on("uncaughtException", () => {
-  console.log(`😈🙉 uncaughtException is detected. Shutting down...`);
-  process.exit(1);
-});
